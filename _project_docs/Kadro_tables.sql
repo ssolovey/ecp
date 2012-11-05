19d7e4d0d9030b02c01d95686a082fe154a4c74a
Sergey Solovey <SSolovey@luxoft.com>
2012-11-05 23:51:48 +0200

    remove unecessary tables

--- _project_docs/Kadro_tables.sql
+++ _project_docs/Kadro_tables.sql
@@ -47,35 +47,3 @@ CREATE TABLE `applic_table` (
   PRIMARY KEY (`applic_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Application table';
 
-
-
-
-
-
-
-
-# Dump of table parts_table
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `parts_table`;
-
-CREATE TABLE `parts_table` (
-  `part_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Applic ID',
-  `part_number` varchar(12) DEFAULT NULL COMMENT 'Path Number',
-  `start_year` int(4) NOT NULL DEFAULT '0' COMMENT 'Start Year',
-  `end_year` int(4) NOT NULL DEFAULT '0' COMMENT 'End Year',
-  `type` boolean NOT NULL DEFAULT 'TRUE' COMMENT 'Transmission',
-  `fluid_option` varchar(1) unsigned NOT NULL COMMENT 'Optionality of Fluid',
-  `fluid_quantity` int(3) unsigned NOT NULL COMMENT 'Fuild Quantity Required',
-  `fuel_type` varchar(8) unsigned NOT NULL COMMENT 'Fuel Type',
-  `engine` varchar(32) unsigned NOT NULL COMMENT 'Engine Sizes',
-  `drive` varchar(8) unsigned NOT NULL COMMENT 'Drive Type',
-  `aspiration` varchar(2) unsigned NOT NULL COMMENT 'Code Letter for Aspiration',
-  `cylinder` varchar(4) unsigned NOT NULL COMMENT 'Cylinder Type',
-  `family` varchar(30) unsigned NOT NULL COMMENT 'Family Type',
-  `MSRP` varchar(30) unsigned NOT NULL COMMENT 'MSRP',
-  `core` varchar(30) unsigned NOT NULL COMMENT 'Core Price',
-  `orwarval` int(10) unsigned NOT NULL COMMENT 'Original Warranty Value',
-  `comwarval` int(10) unsigned NOT NULL COMMENT 'Commercial Warranty Value',
-  PRIMARY KEY (`part_id`),
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Parts table';
