8fe21dea9920acc891920b65e59d617c06b49284
igor.zhavoronkin <izhavoronkin@luxoft.com>
2012-11-07 18:22:18 +0200

    Rename frontend pakage to "reman"/ unecessary files deleted

--- app/code/local/Reman/Quote/controllers/IndexController.php
+++ app/code/local/Reman/Quote/controllers/IndexController.php
@@ -6,10 +6,5 @@ class Reman_Quote_IndexController extends Mage_Core_Controller_Front_Action
         $this->loadLayout();  //This function read all layout files and loads them in memory
         $this->renderLayout(); //This function processes and displays all layout phtml and php files.
 		
-		/* Init templates data*/
-		//$data_make		= Mage::getModel('sync/make');
-		//$data_model		= Mage::getModel('sync/model');
-		//$data_applic	= Mage::getModel('sync/applic');
-		
     }
 }
---
+++ skin/frontend/reman/default/js/general.js
@@ -0,0 +1,13 @@
+/** 
+ *	Resolve conflict with default prototype library
+ *  use #j to define jQuery namespace
+*/
+var $j = jQuery.noConflict();
+
+
+/** DOM Ready Event */
+
+$j('document').ready(function(){
+	
+	
+});
