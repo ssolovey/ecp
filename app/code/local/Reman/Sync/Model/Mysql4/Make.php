52ef71c2ac5388d5c939951c53647dc36ec8fa9d
igor.zhavoronkin <izhavoronkin@luxoft.com>
2012-11-09 21:02:02 +0200

    Add Make id as Value for selectbox option

--- app/code/local/Reman/Sync/Model/Mysql4/Make.php
+++ app/code/local/Reman/Sync/Model/Mysql4/Make.php
@@ -10,8 +10,8 @@ class Reman_Sync_Model_Mysql4_Make extends Mage_Core_Model_Mysql4_Abstract
 	 * SQL query for select makers names from reman_make table
 	*/
 	public function loadMake(){
-        $select = $this->_getReadAdapter()->select()->from('reman_make',array('make')); // form sql query "SELECT make FROM reman_make"
-		$result = $this->_getReadAdapter()->fetchCol($select); // run sql query
+		$select = $this->_getReadAdapter()->select()->from('reman_make',array('make','make_id')); // form sql query "SELECT make,make_id FROM reman_make"
+		$result = $this->_getReadAdapter()->fetchAll($select); // run sql query
 		return $result; // return result
     }
 }
