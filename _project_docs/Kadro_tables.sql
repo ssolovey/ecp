0301d0b8086038a656cf3b28a52e8fcd09f7a80b
Sergey Solovey <SSolovey@luxoft.com>
2012-11-10 23:08:34 +0200

    Updated tables structure for Model and Applic according to review with Artem

--- _project_docs/Kadro_tables.sql
+++ _project_docs/Kadro_tables.sql
@@ -22,7 +22,6 @@ CREATE TABLE `model_table` (
   `vehicle_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Vehicle ID',
   `make_id` int(4) unsigned NOT NULL COMMENT 'Make ID',
   `year` int(4) NOT NULL DEFAULT '0' COMMENT 'Year',
-  `end_year` int(4) NOT NULL DEFAULT '0' COMMENT 'End Year',
   `model` varchar(35) DEFAULT NULL COMMENT 'Model',
   PRIMARY KEY (`vehicle_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Make table';
@@ -39,7 +38,6 @@ CREATE TABLE `applic_table` (
   `applic_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Applic ID',
   `vehicle_id` int(10) unsigned NOT NULL COMMENT 'Vehicle ID',
   `group` int(2) unsigned NOT NULL COMMENT 'Group',
-  `key` int(2) unsigned NOT NULL COMMENT 'Key',
   `subgroup` int(2) unsigned NOT NULL COMMENT 'Subgroup',
   `menu_heading` varchar(30) DEFAULT NULL COMMENT 'Menu Heading',
   `applic` varchar(100) DEFAULT NULL COMMENT 'Applic',
