31392b7d34d4b953427a5cc6a4507a8c934e32ed
Sergey Solovey <SSolovey@luxoft.com>
2012-11-12 00:00:52 +0200

    Add DB dump with all custom tables

--- _project_docs/Kadro_tables.sql
+++ _project_docs/Kadro_tables.sql
@@ -1,40 +1,38 @@
-# Dump of table make_table
+# Dump of table reman_makes
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `make_table`;
+DROP TABLE IF EXISTS `reman_makes`;
 
-CREATE TABLE `make_table` (
+CREATE TABLE `reman_makes` (
   `make_id` int(4) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Make ID',
   `start_year` int(4) NOT NULL DEFAULT '0' COMMENT 'Start Year',
   `end_year` int(4) NOT NULL DEFAULT '0' COMMENT 'End Year',
   `make` varchar(35) DEFAULT NULL COMMENT 'Make',
   PRIMARY KEY (`make_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Make table';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Makes table';
 
 
 
-# Dump of table model_table
+# Dump of table reman_model
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `model_table`;
+DROP TABLE IF EXISTS `reman_model`;
 
-CREATE TABLE `model_table` (
+CREATE TABLE `reman_model` (
   `vehicle_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Vehicle ID',
   `make_id` int(4) unsigned NOT NULL COMMENT 'Make ID',
   `year` int(4) NOT NULL DEFAULT '0' COMMENT 'Year',
   `model` varchar(35) DEFAULT NULL COMMENT 'Model',
   PRIMARY KEY (`vehicle_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Make table';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Models table';
 
 
-
-
-# Dump of table applic_table
+# Dump of table reman_applic
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `applic_table`;
+DROP TABLE IF EXISTS `reman_applic`;
 
-CREATE TABLE `applic_table` (
+CREATE TABLE `reman_applic` (
   `applic_id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Applic ID',
   `vehicle_id` int(10) unsigned NOT NULL COMMENT 'Vehicle ID',
   `group` int(2) unsigned NOT NULL COMMENT 'Group',
@@ -43,5 +41,5 @@ CREATE TABLE `applic_table` (
   `applic` varchar(100) DEFAULT NULL COMMENT 'Applic',
   `part_number` varchar(12) DEFAULT NULL COMMENT 'Path Number',
   PRIMARY KEY (`applic_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Application table';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Applications table';
 
