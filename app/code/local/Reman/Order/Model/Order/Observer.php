ef6a1d6ff3da8a7afdbc5b428a407a7d59ecb6df
Artem Petrosyan <artpetrosyan@gmail.com>
2013-02-01 14:46:14 -0700

    Issue #120: add observer to order creation

---
+++ app/code/local/Reman/Order/Model/Order/Observer.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Order Observer
+ * should call after new order created
+ *
+ * @category    Reman
+ * @package     Reman_Order
+ * @author		Artem Petrosyan (artpetrosyan@gmail.com)
+ */
+class Reman_Order_Model_Order_Observer
+{
+	public function __construct()
+	{
+	
+	}
+	
+	/**
+	 * Exports new orders to an cdv file
+	 *
+	 * @param Varien_Event_Observer $observer
+	 * @return Feed_Sales_Model_Order_Observer
+	 */
+	public function export_new_order($observer)
+	{
+		Mage::log("reached export_new_order");
+		try
+		{
+			$dumpFile = fopen('export/testdump', 'w+');
+			fwrite($dumpFile, 'this is a test!');
+		}
+		catch (Exception $e)
+		{
+			Mage::log("order export failed.\n");
+		}
+		return $this;
+	}
+}
