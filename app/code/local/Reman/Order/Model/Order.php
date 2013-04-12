5ecf0026a19a646559ea2732ff6ba1e471b963cd
Artem Petrosyan <artpetrosyan@gmail.com>
2013-04-12 00:08:18 -0600

    Issue #206: catch and return error durring order creation

--- app/code/local/Reman/Order/Model/Order.php
+++ app/code/local/Reman/Order/Model/Order.php
@@ -33,72 +33,79 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
 			return;	
 		}
 		
-		
-		// init new order
-		$quote = Mage::getModel('sales/quote')
-        	->setStoreId(Mage::app()->getStore('default')->getId());
-		
-		$customer = Mage::getModel('customer/customer')
-                ->setWebsiteId(1)
-                ->load($customer_id);
-        
-        $quote->assignCustomer($customer);
-        
-		// add product(s)
-		$product_id = Mage::getModel('catalog/product')->loadByAttribute( 'sku', $data['partnum'] );
-		
-		$product = Mage::getModel('catalog/product')->load($product_id->getId());
-		
-		$buyInfo = array(
-		        'qty'	=> 1
-		);
-		
-		$quote->addProduct($product, new Varien_Object($buyInfo));
-		
-		$billingData = array(
-			'firstname'		=> $data['bt_cust_name'],
-			'lastname'		=> '_',
-			'street' 		=> array($data['bt_addr1'],$data['bt_addr2']),
-			'city' 			=> $data['bt_city'],
-			'country_id' 	=> 'US',
-			'region_id' 	=> $data['bt_state'],
-			'postcode' 		=> $data['bt_zip'],
-			'telephone' 	=> $data['st_phone']
-		);
-		
-		$shippingData = array(
-			'firstname'		=> $data['st_cust_name'],
-			'lastname'		=> '_',
-			'street' 		=> array($data['st_addr1'],$data['st_addr2']),
-			'city' 			=> $data['st_city'],
-			'country_id' 	=> 'US',
-			'region_id' 	=> $data['st_state'],
-			'postcode' 		=> $data['st_zip'],
-			'telephone' 	=> $data['st_phone']
-		);
-		
-		
-		$billingAddress = $quote->getBillingAddress()->addData($billingData);
-		$shippingAddress = $quote->getShippingAddress()->addData($shippingData);
- 
-		$shippingAddress->setCollectShippingRates(true)->collectShippingRates()
-	        ->setShippingMethod('flatrate_flatrate')
-	        ->setPaymentMethod('checkmo');
-	
+		try
+		{
+			// init new order
+			$quote = Mage::getModel('sales/quote')
+	        	->setStoreId(Mage::app()->getStore('default')->getId());
 			
-		$quote->getPayment()->importData(array('method' => 'checkmo'));
+			$customer = Mage::getModel('customer/customer')
+	                ->setWebsiteId(1)
+	                ->load($customer_id);
+	        
+	        $quote->assignCustomer($customer);
+	        
+			// add product(s)
+			$product_id = Mage::getModel('catalog/product')->loadByAttribute( 'sku', $data['partnum'] );
+			
+			$product = Mage::getModel('catalog/product')->load($product_id->getId());
+			
+			$buyInfo = array(
+			        'qty'	=> 1
+			);
+			
+			$quote->addProduct($product, new Varien_Object($buyInfo));
+			
+			$billingData = array(
+				'firstname'		=> $data['bt_cust_name'],
+				'lastname'		=> '_',
+				'street' 		=> array($data['bt_addr1'],$data['bt_addr2']),
+				'city' 			=> $data['bt_city'],
+				'country_id' 	=> 'US',
+				'region_id' 	=> $data['bt_state'],
+				'postcode' 		=> $data['bt_zip'],
+				'telephone' 	=> $data['st_phone']
+			);
+			
+			$shippingData = array(
+				'firstname'		=> $data['st_cust_name'],
+				'lastname'		=> '_',
+				'street' 		=> array($data['st_addr1'],$data['st_addr2']),
+				'city' 			=> $data['st_city'],
+				'country_id' 	=> 'US',
+				'region_id' 	=> $data['st_state'],
+				'postcode' 		=> $data['st_zip'],
+				'telephone' 	=> $data['st_phone']
+			);
+			
+			
+			$billingAddress = $quote->getBillingAddress()->addData($billingData);
+			$shippingAddress = $quote->getShippingAddress()->addData($shippingData);
+	 
+			$shippingAddress->setCollectShippingRates(true)->collectShippingRates()
+		        ->setShippingMethod('flatrate_flatrate')
+		        ->setPaymentMethod('checkmo');
 		
-		
-		$service = Mage::getModel('sales/service_quote', $quote);
-		$service->submitAll();
-		$order = $service->getOrder();
-		
+				
+			$quote->getPayment()->importData(array('method' => 'checkmo'));
+			
+			
+			$service = Mage::getModel('sales/service_quote', $quote);
+			$service->submitAll();
+			$order = $service->getOrder();
+		}
+		catch (Exception $e)
+		{
+			return $e;
+		}
 		
 		$data['order_id'] = $order->getIncrementId();
 						
 		$this->setData( $data );
 		
 		$this->save();
+		
+		return 'Success';
 		
 		// export new order
 		Mage::getModel('sync/export')->exportOrder($data);
