4aad48f14d7bc5d0a7cce503800854f1aab52465
Artem Petrosyan <artpetrosyan@gmail.com>
2013-06-10 14:23:32 -0600

    Issue #230: Added new fields to order

--- app/code/local/Reman/Order/sql/order_setup/mysql4-install-1.0.0.php
+++ app/code/local/Reman/Order/sql/order_setup/mysql4-install-1.0.0.php
@@ -83,6 +83,9 @@ $installer->run("
 	`total_amount` decimal(10,2) DEFAULT NULL COMMENT 'Total Amount',
 	`transaction_type` varchar(35) DEFAULT NULL COMMENT 'Transaction Type',
 	`commercial_app` varchar(1) DEFAULT NULL COMMENT 'Commercial App',
+	`product_name` varchar(40) DEFAULT NULL COMMENT 'Product Name',
+	`order_notes` varchar(40) DEFAULT NULL COMMENT 'Order Notes',
+	`fluid` varchar(25) DEFAULT NULL COMMENT 'Fluid',
 	PRIMARY KEY (`order_id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 ");
