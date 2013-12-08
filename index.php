6cb214a1b672bd4a0015e6cf6c89781ba6194491
Sergii Solovei <sergii.solovei@gmail.com>
2013-12-08 11:49:37 -0700

    Disable Google Analytics

--- index.php
+++ index.php
@@ -84,6 +84,6 @@ $mageRunCode = isset($_SERVER['MAGE_RUN_CODE']) ? $_SERVER['MAGE_RUN_CODE'] : ''
 /* Run store or run website */
 $mageRunType = isset($_SERVER['MAGE_RUN_TYPE']) ? $_SERVER['MAGE_RUN_TYPE'] : 'store';
 
-include_once("analytics.php");
+#include_once("analytics.php");
 
 Mage::run($mageRunCode, $mageRunType);
