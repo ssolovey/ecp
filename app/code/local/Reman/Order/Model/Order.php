e66ee8b8f5b9da543da58d946af41eaa82af4ac8
Igor Zhavoronkin <izhavoronkin@luxoft.com>
2014-11-03 23:35:11 +0200

    fix internal error 500 on order submit

--- app/code/local/Reman/Order/Model/Order.php
+++ app/code/local/Reman/Order/Model/Order.php
@@ -22,46 +22,49 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
 	 */
 	public function createOrder( $customer_id, $data, $sync )
 	{
+
 		$orderCollection = $this->getCollection();
 		$orderCollection->addFieldToFilter('ete_order_id', $data['ete_order_id']);
-		
+
 		$order = $orderCollection->getFirstItem();
 
+
+
 		//$order = $this->load( $data['order_id'] );
-		
+
 		// check: is order in web database?
 		if ( $order->getId() && $sync ) {
-			
+
 			$this->_updateOrder($order, $data);
-			
+
 			return;
 		}
-		
+
 		try
 		{
 			// init new order
 			$quote = Mage::getModel('sales/quote')
 	        	->setStoreId(Mage::app()->getStore('default')->getId());
-			
+
 			$customer = Mage::getModel('customer/customer')
 	                ->setWebsiteId(1)
 	                ->load($customer_id);
-	                
+
 	        $data['so_cont_email'] = $customer->getEmail();
-	        
+
 	        $quote->assignCustomer($customer);
-	        
+
 			// add product(s)
 			$product_id = Mage::getModel('catalog/product')->loadByAttribute( 'sku', $data['partnum'] );
-			
+
 			$product = Mage::getModel('catalog/product')->load($product_id->getId());
-			
+
 			$buyInfo = array(
 			        'qty'	=> 1
 			);
-			
+
 			$quote->addProduct($product, new Varien_Object($buyInfo));
-			
+
 			$billingData = array(
 				'firstname'		=> $data['bt_cust_name'],
 				'lastname'		=> '_',
@@ -72,7 +75,7 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
 				'postcode' 		=> $data['bt_zip'],
 				'telephone' 	=> $data['st_phone']
 			);
-			
+
 			$shippingData = array(
 				'firstname'		=> $data['st_cust_name'],
 				'lastname'		=> '_',
@@ -83,37 +86,37 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
 				'postcode' 		=> $data['st_zip'],
 				'telephone' 	=> $data['st_phone']
 			);
-			
-			
+
+
 			$billingAddress = $quote->getBillingAddress()->addData($billingData);
 			$shippingAddress = $quote->getShippingAddress()->addData($shippingData);
-	 
+
 			$shippingAddress->setCollectShippingRates(true)->collectShippingRates()
 		        ->setShippingMethod('flatrate_flatrate')
 		        ->setPaymentMethod('checkmo');
-		
-				
+
+
 			$quote->getPayment()->importData(array('method' => 'checkmo'));
-			
-			
+
+
 			$service = Mage::getModel('sales/service_quote', $quote);
 			$service->submitAll();
 			$order = $service->getOrder();
-		
+
 			$data['order_id'] = $order->getIncrementId();
-										
+
 			$this->setData( $data );
-			
+
 			$this->save();
-						
+
 			// export new order
 			Mage::getModel('sync/export')->exportOrder($data);
 
             // Send Email Notification
-            $this->sendEmailNotification($data);
+            $this->sendEmailNotification($data, $customer->getEmail(), $customer->firstname .' '.$customer->lastname);
 
 			return 'Success';
-			
+
 		}
 		catch (Exception $e)
 		{
@@ -134,7 +137,7 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
 		
 		// export order update
 		//Mage::getModel('sync/export')->exportOrder($data);
-	},
+	}
 
 
   /**
@@ -142,7 +145,7 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
     * Send E-mail to  WebOrders@etereman.com
     */
 
-	public function sendEmailNotification($data){
+	public function sendEmailNotification($data, $email, $customer){
 
 	    if($data['commercial_app'] == 0){
             $com_label = 'Non Commercial';
@@ -152,8 +155,6 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
 
         $params = array(
             'order_id' => $data['order_id'],
-            'company'  => $company->name,
-            'client' => $user->firstname .' '.$user->lastname,
 
             /**** Vehicle info *****/
 
@@ -220,8 +221,8 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
                 'email' => 'WebOrders@etereman.com'
             ),
 
-            $user->email,
-            $user->firstname .' '.$user->lastname,
+            $email,
+            $customer,
             'Order Confirmation',
             $params
         );
@@ -238,7 +239,7 @@ class Reman_Order_Model_Order extends Mage_Core_Model_Abstract
                 ),
 
                 'hybridtestmail@gmail.com',
-                $user->firstname .' '.$user->lastname,
+                 $customer,
                 'Order Confirmation',
                 $params
         );
